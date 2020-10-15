fdea6c1dc3f4e56b45fcee621a947a4f7306fb60
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-15 16:26:00 +0200

    fin exo6.1

---
+++ classes06/exo6.1.js
@@ -0,0 +1,21 @@
+class Circle {
+    constructor(xPost, yPost, radius) {
+        this.xPost = xPost;
+        this.yPost = yPost;
+        this.radius = radius
+    }
+
+    move(xOffset, yOffset) {
+        this.xPost + xOffset; 
+        this.yPost + yOffset
+    }
+
+    get surface() {
+        return 2 * Math.PI * this.radius;
+    }
+}
+
+let circle = new Circle(3,2,5);
+console.log(circle.surface);
+circle.move(-1,5);
+console.log(circle);
