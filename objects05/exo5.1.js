f37770242bcf49b6a3994a9e2dbe53509aa59d45
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-15 12:28:58 +0200

    Fin exo 5.1

--- objects05/exo5.1.js
+++ objects05/exo5.1.js
@@ -2,11 +2,11 @@ const readlineSync = require("readline-sync");
 let nameSerie = readlineSync.question('Donnez moi le nom de votre serie ');
 let producYear = readlineSync.question('Donnez moi l annee de votre serie ');
 let numberNameCasting = new Number(readlineSync.question('combien d acteur a-t-il dans la serie '));
+var nameCasting = []; 
 function askTvSerie() {
-    let nameCasting = " "
-    for (let index = 1; index <= numberNameCasting ; index++) {
+    for (let index = 0; index < numberNameCasting ; index++) {
         let memberCasting = readlineSync.question('Qui sont les acteurs ');
-        nameCasting = memberCasting;    
+        nameCasting[index] = memberCasting;    
     }
 
     let infomation = {
