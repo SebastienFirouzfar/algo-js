2c7091de06c2f7117e99e93d90c0825931739aeb
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-19 09:36:15 +0200

    Modification du commentaire

--- extras07/exo7.2.js
+++ extras07/exo7.2.js
@@ -2,12 +2,12 @@
 function calculFibonacci() {
     let nbre1 = 0;
     let nbre2 = 1;
-    let nbre3 ; 
-    for (let indice = 0; indice < 10; indice++) { 
-       nbre3 = nbre1 + nbre2; //addition 0 + 1
-       nbre1 = nbre2; //nbre 2 (1) vers nbre 1 (0) qui devient 1
-       nbre2 = nbre3;
-       console.log(nbre3)
+    let nbre3;
+    for (let indice = 0; indice <= 10; indice++) {
+        nbre3 = nbre1 + nbre2; //addition 0 + 1
+        nbre1 = nbre2; //nbre 2 (1) vers nbre 1 (0) qui devient 1 2 et etc...
+        nbre2 = nbre3; // nbre3 (1 grace à l'addition va vers) nbre2 (1) 
+        console.log(nbre3); 
     }
 }
 
