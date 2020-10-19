c5548e16e97a6cdf2a92d9c99092fb326a3bb715
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-19 23:00:15 +0200

    Modification de l'exo 7.4

--- extras07/exo7.4.js
+++ extras07/exo7.4.js
@@ -1,57 +1,57 @@
 const readlineSync = require("readline-sync");
-let pizzaTab = [];
-let pizzaSave = [];
+var pizzaTab = [];
+//let pizzaSave = [];
 let askArome;
 let removeArome;
 let addArome;
-let finish = " "; 
+let indice = 0;
 
+do {
+    console.log('Hello! Welcome to the Pizza Flavors Manager.');
+    console.log('Please choose your actions');
+    console.log();
+    console.log('1 - List all the pizza flavors');
+    console.log('2 - Add a new pizza flavor');
+    console.log('3 - Remove a pizza flavor')
+    console.log('4 - Exit this program')
+    let number = parseInt(readlineSync.question("Entrer un numéro  : "));
+    console.log();
 
-console.log('Hello! Welcome to the Pizza Flavors Manager.');
-console.log('Please choose your actions');
-console.log();
-console.log('1 - List all the pizza flavors');
-console.log('2 - Add a new pizza flavor');
-console.log('3 - Remove a pizza flavor')
-console.log('4 - Exit this program')
-let number = parseInt(readlineSync.question("Entrer un numéro  : "));
-while(number !== 4){
-    switch(number) {
+    switch (number) {
         case 1:
             console.log("Pas de liste de pizza pour le moment");
             break;
-    
+
         case 2:
-            askArome = readlineSync.question('Combien d arome voulez-vous en ajouter');
-            for (let indice = 0; indice < askArome; indice++) {
-                addArome = readlineSync.question('Quel arome voulez-vous ajouter ?');
+            askArome = parseInt(readlineSync.question('Combien d arome voulez-vous en ajouter '));
+            for (indice; indice < askArome; indice++) {
+                addArome = readlineSync.question('Quel arome voulez-vous ajouter ? ');
                 pizzaTab.push(addArome);
+                //pizzaSave = pizzaTab; sauvegarde la pizza pour la case 3 si le client souhaite effacé un arôme   
+            }
+            console.log(pizzaTab);
+            console.log(); 
+            break;
+
+        case 3:
+            for (let i = 0; i < pizzaTab.length; i++) {
+                console.log(pizzaTab);
+                removeArome =  parseInt(readlineSync.question("Enter your action s number "));
+                //pizzaTab.pop(pizzaTab[removeArome]);
+                pizzaTab.splice(removeArome - 1, 1); //compter à partir de 1 jusqu'à n 
+                console.log("Vous avez enlevé l arome numero " + removeArome);
                 console.log(pizzaTab);
             }
-            finish = readlineSync.question('Avez-vous finis ? '); 
-            if(finish == "oui"){
-                pizzaSave = pizzaTab; // sauvegarde la pizza pour la case 3 si le client souhaite effacé un arôme 
-            }
             break;
-    
-        case 3:
-            for (let i = 0; i < pizzaSave.length; i++) {
-                console.log(pizzaSave + i);
-                removeArome = new Number(readlineSync.question("Enter your action s number "));
-                pizzaTab.pop(pizzaSave[removeArome]);
-                console.log("Vous avez enlevé l arome numero " + removeArome);
-                console.log(pizzaSave);
-            }
-            break;
-    
+
         case 4:
-            console.log("Au revoir");
+            console.log("Au revoir ");
             break;
-        default: console.log("Pizza vide");
+        default: console.log("Pizza vide ");
     }
-}
 
+} while (4);
 
-console.log("le client souhaite une pizza avec " + pizzaSave);
+console.log("le client souhaite une pizza avec " + pizzaTab);
 
 
