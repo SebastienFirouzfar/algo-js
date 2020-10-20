60cc1b6664b7b6da0d8266c9143540d3c6d9f272
Firouzfar <firouzfar.sebastien@gmail.com>
2020-10-20 16:54:51 +0200

    Exo 7.5.js

--- extras07/exo7.5.js
+++ extras07/exo7.5.js
@@ -1,15 +1,14 @@
 let arraySort = [4, 8, 2, 7, 5, 1, 3, 10, 9, 6];
 var temp;
 function trie(arraySort) {
-    for (var j = 0; j < array.length - 1; j++) {
-        for (var k = 1; k < array.length; k++) {
-            if (arraySort[indice] < arraySort[indice + 1]) {
-                temp = arraySort[indice];
-                arraySort[indice] = arraySort[indice + 1];
-                arraySort[indice + 1] = temp;
-            }
-            console.log(temp);
+    for (var indice = 0; indice < arraySort.length - 1; indice++) {
+        if (arraySort[indice] < arraySort[indice + 1]) {
+            temp = arraySort[indice];
+            arraySort[indice] = arraySort[indice + 1];
+            arraySort[indice + 1] = temp;
         }
+        console.log(temp);
     }
 }
-    console.log(trie(arraySort));
+
+console.log(trie(arraySort));
